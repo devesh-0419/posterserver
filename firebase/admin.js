7738c3a7c789eff54b2898fa7dfb676aa5618ef2
Devesh Swarnkar <deveshswarnkar007@gmail.com>
2025-08-02 01:11:40 +0530

    err 7

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,11 +1,9 @@
 // initFirebase.js
 const admin = require('firebase-admin');
-
 require('dotenv').config();
-// console.log('serviceAccount', process.env.SERVICE_ACCOUNT_KEY);
+console.log('serviceAccount', process.env.SERVICE_ACCOUNT_KEY);
 
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY); // downloaded from Firebase Console
-console.log('serviceAccount', serviceAccount);
+const serviceAccount = JSON.parse(Buffer.from(process.env.SERVICE_ACCOUNT_KEY, 'base64').toString()); // downloaded from Firebase Console
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://posterland-165e2-default-rtdb.firebaseio.com"
