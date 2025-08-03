8cd6af696548b013bfa4426f08e1aca56bbcd0c5
Devesh Swarnkar <deveshswarnkar007@gmail.com>
2025-08-03 19:24:51 +0530

    cookie domain

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -6,13 +6,13 @@ const authToken = (payload,res)=>{
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
             expiresIn: "60m",
         });
-
+     console.log('process.env.NODE_ENV', process.env.NODE_ENV);
        return res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "development", // true on prod
+            secure: process.env.NODE_ENV !== "development", // true on prod
             maxAge: 60 * 60 * 1000, // 60 minutes
             sameSite: 'None', // or 'Lax'/'Strict' as needed
-            domain: 'localhost', // or your domain
+            domain: process.env.NODE_ENV === "development" ? 'localhost' : 'https://posterserver.onrender.com', // or your domain
             path: '/',
         });
 }
